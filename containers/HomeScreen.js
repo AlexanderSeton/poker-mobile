8a96c8684bc2c5bf7ee38716d69cba927e719105
Alexander Seton <alexanderseton@protonmail.com>
2022-01-25 20:45:38 +0000

    On user validation, other links become available to click

--- containers/HomeScreen.js
+++ containers/HomeScreen.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, Button } from "react-native";
 
 const HomeScreen = (props) => {
 
+    console.log(props);
+
     const [loggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+        console.log("re-render")
+        if (props.route.params !== undefined) {
+            loggedInStatus = props.route.params.loggedIn;
+            setLoggedIn(loggedInStatus);
+        }
+    })
 
     const checkLoggedIn = () => {
         if (loggedIn) {
@@ -17,33 +27,35 @@ const HomeScreen = (props) => {
             <Button
                 title="Log In"
                 onPress={() =>
-                    props.navigation.navigate("Login")
+                    props.navigation.navigate("Login", {
+                        loggedIn: loggedIn
+                    })
                 }
             />
             <Button
                 title="Join Game"
-                disabled={!checkLoggedIn()}
+                disabled={!loggedIn}
                 onPress={() =>
                     props.navigation.navigate("JoinGame")
                 }
             />
             <Button
                 title="Create Game"
-                disabled={!checkLoggedIn()}
+                disabled={!loggedIn}
                 onPress={() =>
                     props.navigation.navigate("CreateGame")
                 }
             />
             <Button
                 title="Friends"
-                disabled={!checkLoggedIn()}
+                disabled={!loggedIn}
                 onPress={() =>
                     props.navigation.navigate("Friends")
                 }
             />
             <Button
                 title="Account"
-                disabled={!checkLoggedIn()}
+                disabled={!loggedIn}
                 onPress={() =>
                     props.navigation.navigate("Account")
                 }
--- containers/LoginScreen.js
+++ containers/LoginScreen.js
@@ -1,12 +1,67 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, TextInput, StyleSheet, Alert } from "react-native";
 
-const LoginScreen = () => {
+const LoginScreen = (props) => {
+
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = () => {
+        if (username == null || password == null || username == "" || password == "") {
+            Alert.alert(
+                "Missing Fields",
+                "Enter username and password, then press submit again",
+                [
+                    {
+                      text: "Cancel",
+                      style: "cancel"
+                    },
+                    { 
+                        text: "OK", 
+                    }
+                  ]
+            );
+        }
+        else {
+            // check server to validate user
+            props.route.params.loggedIn = true;
+            props.navigation.navigate("Home", {
+                loggedIn: props.route.params.loggedIn
+            })
+        }
+    }
+
     return(
         <View>
-            <Text>Login Screen</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={setUsername}
+                value={username}
+                placeholder="Username..."
+            />
+            <TextInput
+                style={styles.input}
+                onChangeText={setPassword}
+                value={password}
+                placeholder="Password..."
+            />
+            <Button
+                title="Submit"
+                onPress={() => 
+                    handleSubmit()
+                }
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
 
 export default LoginScreen;
