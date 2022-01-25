ea9d172b6fe1964d2b19ea044598d8567d02ab3c
Alexander Seton <alexanderseton@protonmail.com>
2022-01-25 17:56:26 +0000

    Links only touchable/pressable when logged in state set to true

--- containers/HomeScreen.js
+++ containers/HomeScreen.js
@@ -1,29 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, Button } from "react-native";
 
 const HomeScreen = (props) => {
+
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    const checkLoggedIn = () => {
+        if (loggedIn) {
+            return true;
+        }
+        return false;
+    }
+
     return(
         <View>
             <Button
+                title="Log In"
+                onPress={() => 
+                    setLoggedIn(true)
+                }
+            />
+            <Button
                 title="Join Game"
+                disabled={!checkLoggedIn()}
                 onPress={() =>
                     props.navigation.navigate("JoinGame")
                 }
             />
             <Button
                 title="Create Game"
+                disabled={!checkLoggedIn()}
                 onPress={() =>
                     props.navigation.navigate("CreateGame")
                 }
             />
             <Button
                 title="Friends"
+                disabled={!checkLoggedIn()}
                 onPress={() =>
                     props.navigation.navigate("Friends")
                 }
             />
             <Button
                 title="Account"
+                disabled={!checkLoggedIn()}
                 onPress={() =>
                     props.navigation.navigate("Account")
                 }
