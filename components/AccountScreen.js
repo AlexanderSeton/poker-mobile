07ce622c127170e5f6b5a80e4535d1efa34ecab7
Alexander Seton <alexanderseton@protonmail.com>
2022-01-26 13:20:38 +0000

    Created basic Friends and Account screens

--- components/AccountScreen.js
+++ components/AccountScreen.js
@@ -1,12 +1,33 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 const AccountScreen = () => {
+
+    const [username, setUsername] = useState();
+    const [balance, setBalance] = useState();
+
     return(
-        <View>
-            <Text>Account Screen</Text>
+        <View style={styles.main}>
+            <Text style={styles.text}>Username: {username}</Text>
+            <Text style={styles.text}>Balance: £{balance}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    main: {
+        paddingTop: "7.5%",
+    },
+    text: {
+        textAlign: "center",
+        fontSize: 25,
+        padding: "3%",
+        marginBottom: "5%",
+    },
+    smallText: {
+        textAlign: "center",
+        fontSize: 15,
+    },
+});
 
 export default AccountScreen;
--- components/FriendsScreen.js
+++ components/FriendsScreen.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 const FriendsScreen = (props) => {
     return(
-        <View>
-            <Text>Friends Screen</Text>
+        <View style={styles.main}>
+            <Text style={styles.text}>Friends List</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    main: {
+        paddingTop: "7.5%",
+    },
+    text: {
+        textAlign: "center",
+        fontSize: 25,
+        padding: "3%",
+        marginBottom: "5%",
+    },
+    smallText: {
+        textAlign: "center",
+        fontSize: 15,
+    },
+});
 
 export default FriendsScreen;
