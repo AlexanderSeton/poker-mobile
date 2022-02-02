9f91f3939f81e1cf535313d3960f138b305d53d3
Alexander Seton <alexanderseton@protonmail.com>
2022-02-02 00:09:12 +0000

    Display hole cards

---
+++ components/HoleCard.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+const HoleCard = function(props) {
+
+    const [imagePath, setImagePath] = useState("2_of_clubs");
+
+    // useEffect(() => {
+    //     let data = props.HoleCard.split(" ");
+    //     let suit = data[0];
+    //     let value = data[1];
+    //     let path = suit + value;
+    //     setImagePath(path);
+    // }, [props])
+
+    // function getImage() {
+    //     console.log(props.HoleCard);
+    //     let data = props.HoleCard.split(" ");
+    //     let suit = data[0];
+    //     let value = data[1];
+    //     return "2_of_clubs.png";
+    // }
+
+    return(
+        <View style={styles.imageView}>
+            <Image
+                style ={styles.image}
+                source={require("../assets/cards/SPADESTWO.png")}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    imageView: {
+    // display: "flex",
+    // flexDirection: "row",
+    width: "40%",
+    // height: "100%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    // alignItems: "center",
+    // textAlign: "center",
+    // justifyContent: "space-evenly",
+    },
+    image: {
+        resizeMode: "contain",
+        width: null,
+        height: 200,
+        // marginBottom: "5%",
+        // marginTop: "5%",
+    },
+});
+
+export default HoleCard;
